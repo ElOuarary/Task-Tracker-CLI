7d713397b3266b7e26dd2d848a1f986d5c2d50d8
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2024-10-22 00:41:06 +0100

    Adding the add_task funtion to add tasks to the json file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import json
 import os
 import sys
+import time
 
 
 def create_task_file() -> None:
@@ -12,6 +13,39 @@ def create_task_file() -> None:
         with open(file_name, 'w') as file:
             json.dump([], file)
         print(f"{file_name} was created.")
-        return None
+    else:
+        print(f"{file_name} already exists.")
+
+
+def add_task(description:str) -> None:
+    """Add a task to the task.json file"""
+
+    # Try to read the task.json file content
+    try:
+        with open('task.json', 'r') as file:
+            tasks = json.load(file)
+    except json.JSONDecodeError:
+        raise json.JSONDecodeError(f"Error: The file 'task.json' contains invalid JSON data.")
     
-    print(f"{file_name} already exists.")
+    # # Normalize task description to prevent case-sensitive duplicates
+    description_list = [task['description'].lower() for task in tasks]
+    
+    # Check if the task already exists
+    if description.lower() not in description_list:
+        # Create a dictionary that contain all the task information
+        task = {
+            'id': len(tasks) + 1,
+            'description': description,
+            'status': 'todo',
+            'createdAT': time.asctime(),
+            'updatedAT': None
+        }
+        
+        # Apped new task and update the task.json file
+        tasks.append(task)
+        with open('task.json', 'w') as file:
+            json.dump(tasks, file, indent=4)
+        
+        print(f"Task added succesfully.")
+    else:
+        print(f"Task with the description '{description}' already exists!")
