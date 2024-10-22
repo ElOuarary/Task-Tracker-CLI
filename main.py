671cf88871fc0e073fa37c12370f31456f069c5c
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2024-10-22 16:53:10 +0100

    Succefuly adding the create command

--- main.py
+++ main.py
@@ -5,33 +5,40 @@ import sys
 import time
 
 
-def create_task_file() -> None:
+def create_task_file(file_name:str) -> None:
     '''Create a JSON task file if it does not exists'''
-    file_name = "task.json"
+
+    # Check if the file ends with json
+    if not file_name.endswith(".json"):
+        print(f"{file_name} is not json file.")
     
-    if not os.path.exists(file_name):
+    # Check if the file exists
+    elif not os.path.exists(file_name):
         with open(file_name, 'w') as file:
             json.dump([], file)
         print(f"{file_name} was created.")
+    
+    # print a messaget that indicate that the file already exists
     else:
         print(f"{file_name} already exists.")
 
 
-def add_task(description:str) -> None:
+def add_task(description:str, file_name:str) -> None:
     """Add a task to the task.json file"""
 
     # Try to read the task.json file content
     try:
-        with open('task.json', 'r') as file:
+        with open(file_name, 'r') as file:
             tasks = json.load(file)
     except json.JSONDecodeError:
-        raise json.JSONDecodeError(f"Error: The file 'task.json' contains invalid JSON data.")
+        raise json.JSONDecodeError(f"Error: The file '{file_name}' contains invalid JSON data.")
     
     # # Normalize task description to prevent case-sensitive duplicates
     description_list = [task['description'].lower() for task in tasks]
     
     # Check if the task already exists
     if description.lower() not in description_list:
+
         # Create a dictionary that contain all the task information
         task = {
             'id': len(tasks) + 1,
@@ -43,9 +50,25 @@ def add_task(description:str) -> None:
         
         # Apped new task and update the task.json file
         tasks.append(task)
-        with open('task.json', 'w') as file:
+        with open(file_name, 'w') as file:
             json.dump(tasks, file, indent=4)
         
         print(f"Task added succesfully.")
     else:
         print(f"Task with the description '{description}' already exists!")
+
+
+def main():
+    commands = sys.argv[1:]
+    match commands[0]:
+        case 'create':
+            if len(commands) > 2:
+                print(f"Commande 'create' supports only one argument.")
+            elif len(commands) == 2:
+                create_task_file(commands[1])
+            else:
+                create_task_file('task.json')
+        case _:
+            print(f"Invalid Command")
+
+main()
