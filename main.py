c78e04538af1c2f30a922ea9b87c0d292b53fa66
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2024-10-26 17:01:03 +0100

    added the update-task function and handled different case scenario for both update-task and add-task

--- main.py
+++ main.py
@@ -3,46 +3,48 @@ import json
 import os
 import sys
 import time
-from typing import Any
 
 
-def create_task_file(file_name:str) -> None:
+def create_task_file() -> None:
     '''Create a JSON task file if it does not exists'''
 
-    # Check if the file ends with json
-    if not file_name.endswith(".json"):
-        print(f"{file_name} is not json file.")
-    
     # Check if the file exists
-    elif not os.path.exists(file_name):
-        with open(file_name, 'w') as file:
+    if not os.path.exists("task.json"):
+        with open("task.json", 'w') as file:
             json.dump([], file)
-        print(f"{file_name} was created.")
+        print(f"task.json file was created.")
     
     # print a messaget that indicate that the file already exists
     else:
-        print(f"{file_name} already exists.")
+        print(f"task.json file already exists.")
 
 
-def task_exists(description:str, tasks:list) -> bool:
-    """Check for the existence of the task in the tasks list"""
+def description_exists(description: str, tasks: list) -> bool:
+    """Check for the existence of the description in the tasks list"""
     # Normalize task description to prevent case-sensitive duplicates
     description_list = [task['description'].lower() for task in tasks]
-    return description.lower() not in description_list
+    return description.lower() in description_list
 
 
-def add_task(description:str, file_name:str) -> None:
+def id_exists(id: int, tasks: list) -> bool:
+    """Check for the existence of the id in the tasks list"""
+    id_list = [task["id"] for task in tasks]
+    return id in id_list
+
+
+def add_task(description: str) -> None:
     """Add a task to the task.json file"""
 
-    # Try to read the task.json file content
+    # Try to read the task.json file
     try:
-        with open(file_name, 'r') as file:
-            tasks:list = json.load(file)
-    except json.JSONDecodeError:
-        raise json.JSONDecodeError(f"Error: The file '{file_name}' contains invalid JSON data.")
+        with open("task.json", 'r') as file:
+            tasks: list = json.load(file)
+    except FileNotFoundError:
+        print(f"Error: The file 'task.json' does not exists.")
+        return None
     
-    # Check if the task already exists
-    if task_exists(description, tasks):
+    # Check if the task does not exist
+    if not description_exists(description, tasks):
 
         # Create a dictionary that contain all the task information
         task = {
@@ -55,33 +57,81 @@ def add_task(description:str, file_name:str) -> None:
         
         # Apped new task and update the task.json file
         tasks.append(task)
-        with open(file_name, 'w') as file:
+        with open("task.json", 'w') as file:
             json.dump(tasks, file, indent=4)
         
         print(f"Task added succesfully.")
     else:
-        print(f"Task with the description '{description}' already exists!")
+        print(f"Error: Task with the description '{description}' already exists!")
 
+
+def update_task(id: int, new_description: str):
+    """Update a task in task.json file"""
+
+    # Try to read the task.json file
+    try:
+        with open("task.json", 'r') as file:
+            tasks: list = json.load(file)
+    except FileNotFoundError:
+        print(f"Error: The file 'task.json' does not exists.")
+
+    if id_exists(id, tasks):
+        tasks[id-1]["description"] = new_description
+        tasks[id-1]["updatedAT"] = time.asctime()
+    
+        # Apped new task and update the task.json file
+        with open("task.json", 'w') as file:
+           json.dump(tasks, file, indent=4)
+        print(f"Task with the id:{id} updated succefully.")
+    else:
+        print(f"Error: No task with the id:{id} exists!") 
+    
 
 def main():
     try:
         commands = sys.argv[1:]
         commands_length = len(commands)
+        
+        # Handle different commandes
         match commands[0]:
+
+            # Command 'create' workflow
             case 'create':
                 if commands_length > 1:
                     print(f"Commande 'create' does not support any parameter.")
                 else:
-                    create_task_file('task.json')
-            case 'add_task':
+                    create_task_file()
+                    sys.exit()
+
+            # Command 'add-task' workflow        
+            case 'add-task':
                 if commands_length > 2:
-                    print(f"Commande 'add_task' supports only one parameter.")
-                elif commands_length == 0:
-                    print(f"Commade 'add_task' needs additional parameter.")
+                    print(f"Commande 'add-task' supports only one parameter.")
+                elif commands_length == 1:
+                    print(f"1 missing parameter.")
                 else:    
-                    add_task(commands[1], 'task.json')
+                    add_task(commands[1])
+                    sys.exit()
+
+            # Command 'update-task workflow
+            case "update-task":
+                if commands_length > 3:
+                    print(f"Commande 'update-task' only supports 2 parametrs.")
+                elif commands_length == 2:
+                    print(f"1 missing parameter!")
+                elif commands_length == 1:
+                    print(f"2 missing parametes!")
+                else:
+                    if not commands[1].isdigit():
+                        print(f"Value Error: first parameter is not an integer!")
+                        sys.exit()    
+                    update_task(int(commands[1]), commands[2])
+                    sys.exit()
+
+            # Handle a non existing command
             case _:
-                print(f"Invalid Command")
+                print(f"Invalid Command!")
+    
     except:
         return None
         
